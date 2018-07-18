7fcc317d421bec99e3a61ea4e40eb7835c4f91a2
MuhanguziDavid <muhunksdavie95@gmail.com>
2018-07-19 01:28:07 +0300

    carryout tests for put endpoint

--- tests/test_diary_entries.py
+++ tests/test_diary_entries.py
@@ -22,13 +22,15 @@ class TestDiaryEntries(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_non_existing_entry(self):
-        """tests whether it will returen status code 404 (not found)"""
+        """tests whether it will returen status code 404 (not found)
+        if a user tries to retreive non existing entry
+        """
         _id = {"entry_id": 9}
         response = self.myapp.get('/entry/{}'.format(_id['entry_id']))
         self.assertEqual(response.status_code, 404)
 
     def test_post_new_entry(self):
-        """tests whether it will returen status code 201 (created)"""
+        """tests whether new entry will be created"""
         response = self.myapp.post('/entry/5',
                                    data=json.dumps(dict(
                                        title='The year 1995',
@@ -37,9 +39,11 @@ class TestDiaryEntries(unittest.TestCase):
                                    content_type='application/json')
 
         self.assertEqual(response.status_code, 201)
-    
+
     def test_post_with_existing_id(self):
-        """tests whether it will returen status code 400 (bad request)"""
+        """tests whether it will returen status code 400 (bad request)
+        if an existing entry_id is entered
+        """
         response = self.myapp.post('/entry/3',
                                    data=json.dumps(dict(
                                        title='The year 1995',
@@ -48,6 +52,19 @@ class TestDiaryEntries(unittest.TestCase):
                                    content_type='application/json')
 
         self.assertEqual(response.status_code, 400)
+    
+    def test_put_existing_entry(self):
+        """tests whether an entry will be updates
+        returns status code 200 (okay)
+        """
+        response = self.myapp.put('/entry/3',
+                                   data=json.dumps(dict(
+                                       title='This week',
+                                       description='I started practicing flask'
+                                   )),
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
