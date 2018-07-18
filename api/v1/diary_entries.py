aeefdcf17fd57e781d13d2fa47947f600ce2e15c
MuhanguziDavid <muhunksdavie95@gmail.com>
2018-07-18 23:53:04 +0300

    add post new entry endpoint

--- api/v1/diary_entries.py
+++ api/v1/diary_entries.py
@@ -1,6 +1,6 @@
 """Get all entries in MyDiary"""
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -26,16 +26,42 @@ entries = [
 
 class Entry(Resource):
     """Class for Entry resource"""
+    parser = reqparse.RequestParser()
+    parser.add_argument('title',
+                        type=str,
+                        required=True,
+                        help="This field can not be left blank!"
+                        ),
+    parser.add_argument('description',
+                        type=str,
+                        required=True,
+                        help="This field can not be left blank!"
+                        )
+
     def get(self, entry_id):
-        entry = next(filter(lambda x: x['entry_id'] == entry_id, entries), None)
+        entry = next(
+            filter(lambda x: x['entry_id'] == entry_id, entries), None)
         return {'entry': entry}, 200 if entry else 404
+
+    def post(self, entry_id):
+        if next(filter(lambda x: x['entry_id'] == entry_id, entries), None):
+            return {'message': "An item with id '{}' already exists.".format(entry_id)}, 400
+
+        data = Entry.parser.parse_args()
+
+        entry = {'entry_id': entry_id,
+                 'title': data['title'], 'description': data['description']}
+        entries.append(entry)
+        return entry, 201
 
 
 class EntryList(Resource):
     """Class for EntryList resource"""
+
     def get(self):
         """method to return all entries"""
         return {'entries': entries}
+
 
 api.add_resource(Entry, '/entry/<int:entry_id>')
 api.add_resource(EntryList, '/entries')
