fa72bffca00928746c0cc3d2a68fbfb93dbcdf08
MuhanguziDavid <muhunksdavie95@gmail.com>
2018-08-13 04:17:09 +0300

    add fetch for signin

---
+++ UI/js/global.js
@@ -0,0 +1,2 @@
+var host = "http://127.0.0.1:5000";
+// var host = "https://my-diary-3.herokuapp.com";
---
+++ UI/js/signup.js
@@ -0,0 +1,26 @@
+function submit_user(){
+    var url = "http://127.0.0.1:5000/api/v1/auth/signup";
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+            "username": document.getElementById("username").value,
+            "email": document.getElementById("email").value,
+            "password": document.getElementById("password").value,
+            "confirm_password": document.getElementById("confirm_password").value,
+        })
+    })
+    .then((response) => response.json())
+    .then(function(data){
+        if(data.message == "Account Created Successfully"){
+            window.location.href = "index.html";
+        }else{
+            console.log("Data ",data.message)
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
