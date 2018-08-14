156f9e1591bfaca40c5d91bbe0cf0093ae63f3d0
MuhanguziDavid <muhunksdavie95@gmail.com>
2018-08-15 02:03:24 +0300

    add code to test cookie

--- UI/js/cookies.js
+++ UI/js/cookies.js
@@ -1,8 +1,24 @@
-function setCookie(cookie_name, cookie_value, exdays) {
+function set_cookie(cookie_name, cookie_value, exdays) {
     var date = new Date();
     date.setTime(date.getTime()+ (exdays*24*60*60*1000));
     expires = date.toGMTString();
     document.cookie = cookie_name + "=" + cookie_value + ";expires=" + expires + ";path=/";
+    var mycookie = document.cookie;
+    return mycookie;
 }
 
-
+function get_cookie(cookie_name) {
+    var name = cookie_name + "=";
+    var decoded_cookie = decodeURIComponent(document.cookie);
+    var many_cookies = decoded_cookie.split(';');
+    for(var i = 0; i < many_cookies.length; i++) {
+        var single_cookie = many_cookies[i];
+        while (single_cookie.charAt(0) === ' ') {
+            single_cookie = single_cookie.substring(1,single_cookie.length);
+        }
+        if (single_cookie.indexOf(name) === 0) {
+            return single_cookie.substring(name.length, single_cookie.length);
+        }
+    }
+    return "no";
+}
--- UI/js/users.js
+++ UI/js/users.js
@@ -3,7 +3,7 @@ function register_user(){
     fetch(url, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             "username": document.getElementById("username").value,
@@ -17,7 +17,7 @@ function register_user(){
     })
     .then(function(data){
         if(data.message == "Account Created Successfully"){
-            setCookie("auth_token", data.auth_token, 1);
+            set_cookie("auth_token", data.auth_token, 1);
             window.location.href = "home.html";
             // post_login_message("Logged in Successfully, Welcome");
         }else{
@@ -35,7 +35,7 @@ function login_user(){
     fetch(url, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             "username": document.getElementById("username").value,
@@ -47,9 +47,10 @@ function login_user(){
     })
     .then(function(data){
         if(data.message == "Logged in Successfully"){
-            setCookie("auth_token", data.auth_token, 1);
-            window.location.href = "home.html";
-            // post_login_message("Logged in Successfully, Welcome");
+            let setthecookie = set_cookie("auth_token", data.auth_token, 1);
+            //window.location.href = "home.html";
+            console.log(setthecookie);
+            //post_login_message("Logged in Successfully, Welcome");
         }else{
             document.getElementById('login_feedback').innerHTML = "Feedback: " + data.message;
             console.log("Feedback: ",data.message);
@@ -60,6 +61,30 @@ function login_user(){
     });
 }
 
-// function post_login_message(message){
-//     document.getElementById('home_feedback').innerHTML = message;
+// function post_login_message(){
+//     document.getElementById('home_feedback').innerHTML = "welcome";
 // }
+
+function get_all_entries(){
+    var url = host_name + "/api/v1/entries";
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'authorization':'Bearer '+get_cookie("auth_token")
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        if(data.status == "success"){
+            console.log(data);
+        }else{
+            console.log("Feedback: ",data);
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
